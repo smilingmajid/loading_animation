dc23efedbb4745a8fefd4de562cb9a2f0c8c565f
majid <majid.gha3mi.p@gmail.com>
2025-09-01 14:39:05 +0330

    home comment

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Loading Animation',
 
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
