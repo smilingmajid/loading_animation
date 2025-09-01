b8c5b6056c532a01078d8e9612cfcc355040415a
majid <majid.gha3mi.p@gmail.com>
2025-09-01 14:38:22 +0330

    title change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Loading Animation',
 
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
