3891dcf64c5de5baa7ded3f5837c5fbc440b7332
majid <majid.gha3mi.p@gmail.com>
2025-09-02 17:28:55 +0330

    sroll child view add

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
-    
-class HomeScreen extends StatelessWidget {
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-  const HomeScreen({ super.key });
-  
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final List<Map<String, dynamic>> lAWList = [
+    {
+      "name": "twistingDots",
+      "widget":
+          () => LoadingAnimationWidget.twistingDots(
+            leftDotColor: Colors.white,
+            rightDotColor: Colors.white,
+            size: 50,
+          ),
+    },
+    {
+      "name": "waveDots",
+      "widget":
+          () => LoadingAnimationWidget.waveDots(color: Colors.white, size: 50),
+    },
+    {
+      "name": "inkDrop",
+      "widget":
+          () => LoadingAnimationWidget.inkDrop(color: Colors.white, size: 50),
+    },
+    {
+      "name": "twistingDots",
+      "widget":
+          () => LoadingAnimationWidget.twistingDots(
+            leftDotColor: Colors.white,
+            rightDotColor: Colors.white,
+            size: 50,
+          ),
+    },
+    {
+      "name": "fallingDot",
+      "widget":
+          () =>
+              LoadingAnimationWidget.fallingDot(color: Colors.white, size: 50),
+    },
+    {
+      "name": "flickr",
+      "widget":
+          () => LoadingAnimationWidget.flickr(
+            leftDotColor: Colors.white,
+            rightDotColor: Colors.white,
+            size: 50,
+          ),
+    },
+    {
+      "name": "beat",
+      "widget":
+          () => LoadingAnimationWidget.beat(color: Colors.white, size: 50),
+    },
+    {
+      "name": "dotsTriangle",
+      "widget":
+          () => LoadingAnimationWidget.dotsTriangle(
+            color: Colors.white,
+            size: 50,
+          ),
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      backgroundColor: const Color.fromARGB(115, 50, 50, 50),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 1,
+                ),
+                itemCount: lAWList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: const Color.fromARGB(150, 30, 30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        lAWList[index]["widget"](),
+                        const SizedBox(height: 8),
+                        Text(
+                          lAWList[index]["name"],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
