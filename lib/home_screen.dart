6ee8ce1bc25a4ed4361c601ba2a7537b7d2f3742
majid <majid.gha3mi.p@gmail.com>
2025-09-02 22:55:03 +0330

    Colors change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,55 +9,66 @@ class HomeScreen extends StatelessWidget {
       "name": "twistingDots",
       "widget":
           () => LoadingAnimationWidget.twistingDots(
-            leftDotColor: Colors.white,
-            rightDotColor: Colors.white,
+            leftDotColor: const Color.fromARGB(255, 255, 0, 0),
+            rightDotColor: const Color.fromARGB(255, 255, 0, 0),
             size: 50,
           ),
     },
     {
       "name": "waveDots",
       "widget":
-          () => LoadingAnimationWidget.waveDots(color: Colors.white, size: 50),
+          () => LoadingAnimationWidget.waveDots(
+            color: const Color.fromARGB(255, 0, 106, 255),
+            size: 50,
+          ),
     },
     {
       "name": "inkDrop",
       "widget":
-          () => LoadingAnimationWidget.inkDrop(color: Colors.white, size: 50),
+          () => LoadingAnimationWidget.inkDrop(
+            color: const Color.fromARGB(255, 17, 255, 0),
+            size: 50,
+          ),
     },
     {
       "name": "twistingDots",
       "widget":
           () => LoadingAnimationWidget.twistingDots(
-            leftDotColor: Colors.white,
-            rightDotColor: Colors.white,
+            leftDotColor: const Color.fromARGB(255, 255, 0, 221),
+            rightDotColor: const Color.fromARGB(255, 255, 0, 221),
             size: 50,
           ),
     },
     {
       "name": "fallingDot",
       "widget":
-          () =>
-              LoadingAnimationWidget.fallingDot(color: Colors.white, size: 50),
+          () => LoadingAnimationWidget.fallingDot(
+            color: Color(0xfff5004F),
+            size: 50,
+          ),
     },
     {
       "name": "flickr",
       "widget":
           () => LoadingAnimationWidget.flickr(
-            leftDotColor: Colors.white,
-            rightDotColor: Colors.white,
+            leftDotColor: Color(0xffFFAF00),
+            rightDotColor: Color(0xffFFAF00),
             size: 50,
           ),
     },
     {
       "name": "beat",
       "widget":
-          () => LoadingAnimationWidget.beat(color: Colors.white, size: 50),
+          () => LoadingAnimationWidget.beat(
+            color: Color.fromARGB(255, 255, 238, 0),
+            size: 50,
+          ),
     },
     {
       "name": "dotsTriangle",
       "widget":
           () => LoadingAnimationWidget.dotsTriangle(
-            color: Colors.white,
+            color: Color.fromARGB(255, 157, 0, 255),
             size: 50,
           ),
     },
