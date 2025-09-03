39310868ef4869e38cf51f183dbe831532beaf6f
majid <majid.gha3mi.p@gmail.com>
2025-09-03 19:07:15 +0330

    flutter spin kit list  removed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 import 'package:loaging_animation/data/loding_animation_widget_list.dart';
 import 'package:loaging_animation/widget/animation_grid_widget.dart';
 
@@ -7,51 +7,7 @@ import 'package:loaging_animation/widget/animation_grid_widget.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  List<Map<String, dynamic>> flutterspinkitlist = [
-    {
-      "name": "DoubleBounce",
-      "widget": SpinKitDoubleBounce(
-        color: const Color.fromARGB(255, 254, 0, 0),
-        size: 50.0,
-      ),
-    },
 
-    {
-      "name": "RotatingCircle",
-      "widget": SpinKitRotatingCircle(
-        color: const Color.fromARGB(255, 170, 1, 170),
-        size: 50.0,
-      ),
-    },
-    {
-      "name": "Circle",
-      "widget": SpinKitCircle(
-        color: const Color.fromARGB(255, 255, 0, 106),
-        size: 50.0,
-      ),
-    },
-    {
-      "name": "ChasingDots",
-      "widget": SpinKitChasingDots(
-        color: const Color.fromARGB(255, 0, 213, 255),
-        size: 50.0,
-      ),
-    },
-    {
-      "name": "PumpingHeart",
-      "widget": SpinKitPumpingHeart(
-        color: const Color.fromARGB(255, 0, 26, 255),
-        size: 50.0,
-      ),
-    },
-    {
-      "name": "DualRing",
-      "widget": SpinKitDualRing(
-        color: const Color.fromARGB(255, 238, 255, 0),
-        size: 50.0,
-      ),
-    },
-  ];
 
   @override
   Widget build(BuildContext context) {
