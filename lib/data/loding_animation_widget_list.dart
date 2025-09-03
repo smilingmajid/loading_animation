2c2f7d5ff57c133de8a12ce20e4423623b4a9298
majid <majid.gha3mi.p@gmail.com>
2025-09-03 19:39:48 +0330

    cl

--- lib/data/loding_animation_widget_list.dart
+++ lib/data/loding_animation_widget_list.dart
@@ -1,74 +1,61 @@
-//class LodingAnimationWidget 
-  import 'dart:ui';
+//class LodingAnimationWidget
+import 'dart:ui';
 
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 final List<Map<String, dynamic>> lodingAnimationList = [
-    {
-      "name": "twistingDots",
-      "widget":
-          () => LoadingAnimationWidget.twistingDots(
-            leftDotColor: const Color.fromARGB(255, 255, 0, 0),
-            rightDotColor: const Color.fromARGB(255, 255, 0, 0),
-            size: 50,
-          ),
-    },
-    {
-      "name": "waveDots",
-      "widget":
-          () => LoadingAnimationWidget.waveDots(
-            color: const Color.fromARGB(255, 0, 106, 255),
-            size: 50,
-          ),
-    },
-    {
-      "name": "inkDrop",
-      "widget":
-          () => LoadingAnimationWidget.inkDrop(
-            color: const Color.fromARGB(255, 17, 255, 0),
-            size: 50,
-          ),
-    },
-    {
-      "name": "twistingDots",
-      "widget":
-          () => LoadingAnimationWidget.twistingDots(
-            leftDotColor: const Color.fromARGB(255, 255, 0, 221),
-            rightDotColor: const Color.fromARGB(255, 255, 0, 221),
-            size: 50,
-          ),
-    },
-    {
-      "name": "fallingDot",
-      "widget":
-          () => LoadingAnimationWidget.fallingDot(
-            color: Color(0xfff5004F),
-            size: 50,
-          ),
-    },
-    {
-      "name": "flickr",
-      "widget":
-          () => LoadingAnimationWidget.flickr(
-            leftDotColor: Color(0xffFFAF00),
-            rightDotColor: Color(0xffFFAF00),
-            size: 50,
-          ),
-    },
-    {
-      "name": "beat",
-      "widget":
-          () => LoadingAnimationWidget.beat(
-            color: Color.fromARGB(255, 255, 238, 0),
-            size: 50,
-          ),
-    },
-    {
-      "name": "dotsTriangle",
-      "widget":
-          () => LoadingAnimationWidget.dotsTriangle(
-            color: Color.fromARGB(255, 157, 0, 255),
-            size: 50,
-          ),
-    },
-  ];
+  {
+    "name": "twistingDots",
+    "widget":
+        () => LoadingAnimationWidget.twistingDots(
+          leftDotColor: const Color.fromARGB(255, 255, 0, 0),
+          rightDotColor: const Color.fromARGB(255, 255, 0, 0),
+          size: 50,
+        ),
+  },
+  {
+    "name": "waveDots",
+    "widget":
+        () => LoadingAnimationWidget.waveDots(
+          color: const Color.fromARGB(255, 0, 106, 255),
+          size: 50,
+        ),
+  },
+  {
+    "name": "inkDrop",
+    "widget":
+        () => LoadingAnimationWidget.inkDrop(
+          color: const Color.fromARGB(255, 17, 255, 0),
+          size: 50,
+        ),
+  },
+  {
+    "name": "twistingDots",
+    "widget":
+        () => LoadingAnimationWidget.twistingDots(
+          leftDotColor: const Color.fromARGB(255, 255, 0, 221),
+          rightDotColor: const Color.fromARGB(255, 255, 0, 221),
+          size: 50,
+        ),
+  },
+
+  {
+    "name": "fallingDot",
+    "widget":
+        () => LoadingAnimationWidget.fallingDot(
+          color: Color(0xfff5004F),
+          size: 50,
+        ),
+  },
+
+  {
+    "name": "flickr",
+    "widget":
+        () => LoadingAnimationWidget.flickr(
+          leftDotColor: Color(0xffFFAF00),
+          rightDotColor: Color(0xffFFAF00),
+          size: 50,
+        ),
+  },
+
+];
