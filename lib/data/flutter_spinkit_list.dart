45674c94954263e31fb79f0ad9468b808e3a3871
majid <majid.gha3mi.p@gmail.com>
2025-09-03 19:09:47 +0330

    flutter spinkit list created

---
+++ lib/data/flutter_spinkit_list.dart
@@ -0,0 +1,49 @@
+  import 'dart:ui';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+List<Map<String, dynamic>> flutterspinkitlist = [
+    {
+      "name": "DoubleBounce",
+      "widget": SpinKitDoubleBounce(
+        color: const Color.fromARGB(255, 254, 0, 0),
+        size: 50.0,
+      ),
+    },
+
+    {
+      "name": "RotatingCircle",
+      "widget": SpinKitRotatingCircle(
+        color: const Color.fromARGB(255, 170, 1, 170),
+        size: 50.0,
+      ),
+    },
+    {
+      "name": "Circle",
+      "widget": SpinKitCircle(
+        color: const Color.fromARGB(255, 255, 0, 106),
+        size: 50.0,
+      ),
+    },
+    {
+      "name": "ChasingDots",
+      "widget": SpinKitChasingDots(
+        color: const Color.fromARGB(255, 0, 213, 255),
+        size: 50.0,
+      ),
+    },
+    {
+      "name": "PumpingHeart",
+      "widget": SpinKitPumpingHeart(
+        color: const Color.fromARGB(255, 0, 26, 255),
+        size: 50.0,
+      ),
+    },
+    {
+      "name": "DualRing",
+      "widget": SpinKitDualRing(
+        color: const Color.fromARGB(255, 238, 255, 0),
+        size: 50.0,
+      ),
+    },
+  ];
